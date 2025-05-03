334f1943fb32f453b07524ab86ee55d874bd3c08
pedram <pedramalizade03@gmail.com>
2025-05-03 12:26:01 +0330

    Add Dockerfile for deployment

--- ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d61e2564ad3c1889cdd364bb7e165d0d5eee24")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
